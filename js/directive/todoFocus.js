1df5dda441f20f44c9bf70d0be82a503deccf984
krezzsy <krezzsy@gmail.com>
2017-03-18 11:09:49 +0300

    add todoFocus

---
+++ js/directive/todoFocus.js
@@ -0,0 +1,17 @@
+/*global angular */
+
+
+angular.module('to-do-list')
+	.directive('todoFocus', function todoFocus($timeout) {
+		'use strict';
+
+		return function (scope, elem, attrs) {
+			scope.$watch(attrs.todoFocus, function (newVal) {
+				if (newVal) {
+					$timeout(function () {
+						elem[0].focus();
+					}, 0, false);
+				}
+			});
+		};
+	});
